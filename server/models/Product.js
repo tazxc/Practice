d13e327520308188beaee7c26f885e16aaa4802b
Nikita Skorobogatov <skytaood@gmail.com>
2023-05-22 18:51:29 +0200

    22.05.23

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,12 +1,12 @@
 const {Schema, model} = require('mongoose')
-const productInfo = require('./productInfo')
+
 
 const ProductSchema = new Schema({
     name:{type: String, required: true},
     price:{type: Number, required: true},
     img:{type: String, required: true},
     typeId: {type: String, required: true},
-    productInfo: {type: String}
+    productInfo: [{type: String, ref: "ProductInfo"}]
 })
 
 module.exports = model('Product', ProductSchema)
