3e2a5ccd284c3253cc8743b31baf31b5a4550741
Nikita Skorobogatov <skytaood@gmail.com>
2023-05-22 13:28:56 +0200

    day 3

--- server/models/productInfo.js
+++ server/models/productInfo.js
@@ -1,7 +1,8 @@
 const {Schema, model} = require('mongoose')
 
+
 const ProductInfoSchema = new Schema({
-    product_id:{type: Schema.Types.ObjectId, ref: 'product'},
+    product_id:{type: Schema.Types.ObjectId, ref: 'Product'},
     title: {type: String, required: true}
 })
 
--- server/models/type.js
+++ server/models/type.js
@@ -1,5 +1,6 @@
 const {Schema, model} = require('mongoose')
 
+
 const TypeSchema = new Schema({
     name:{type: String, required: true}
 })
