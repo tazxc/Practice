38e8830765b08532572e6c153d01937666f410b7
Nikita Skorobogatov <skytaood@gmail.com>
2023-05-24 22:02:47 +0200

    24.05.23

--- client/src/page/Admin.js
+++ client/src/page/Admin.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Router } from 'react-router-dom'
 
 const Admin = () =>{
     return(
         <BrowserRouter>
-            Shop
+            Admin
         </BrowserRouter>
     )
 }
