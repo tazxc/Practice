2c4bc9234b93084c1ef3eb6befc3f23e00804a83
Nikita Skorobogatov <skytaood@gmail.com>
2023-06-24 19:39:13 +0200

    rewrite prodStore

--- client/src/components/ProductItem.js
+++ client/src/components/ProductItem.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Card, Col, Image } from "react-bootstrap";
 import {useNavigate} from "react-router-dom"
-
+import { PRODUCT_ROUTE } from "../utils/consts";
 
 const ProductItem = ({product}) => {
     const history = useNavigate()
     
-    console.log(history)
     return(
-        <Col md={3} className="mt-4">
+        <Col md={3} className="mt-4" onClick={() => history(PRODUCT_ROUTE + '/' + product.id)}>
             <Card style={{width: 150, cursor: 'pointer'}} border={"light"}>
                 <Image width={150} height={150} src={product}/>
                 <div className="mt-1">
